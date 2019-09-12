0d62d62b41b0a2c768b33c493173bfc4b208fe8b
Enzie Riddle <enzieriddle@gmail.com>
2019-09-12 13:25:50 -0500

    Align main header, dashboard header, and charts.

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ let trafficData = {
   datasets: [
     {
       data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
-      backgroundColor: "rgba(116, 119, 191, .3)",
+      backgroundColor: "rgba(255, 222, 222, .5)",
       borderWidth: 1
     }
   ]
@@ -76,7 +76,7 @@ const dailyData = {
     {
       label: "# of Hits",
       data: [75, 115, 175, 125, 225, 200, 100],
-      backgroundColor: "#7477BF",
+      backgroundColor: "#ffdede",
       borderWidth: 1
     }
   ]
@@ -111,7 +111,7 @@ const mobileData = {
       label: "# of Users",
       data: [2000, 550, 500],
       borderWidth: 0,
-      backgroundColor: ["#7477BF", "#78CF82", "#51B6C8"]
+      backgroundColor: ["#ffdede", "#e6c1c1", "#d1a7a7"]
     }
   ]
 };
@@ -130,4 +130,21 @@ let mobileChart = new Chart(mobileCanvas, {
   type: "doughnut",
   data: mobileData,
   options: mobileOptions
+});
+
+/* Messaging Section --------------------------*/
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+send.addEventListener("click", () => {
+  if (user.value === "" && message.value === "") {
+    alert("Required fields: User and Message");
+  } else if (user.value === "") {
+    alert("Required field: User");
+  } else if (message.value === "") {
+    alert("Required field: Message");
+  } else {
+    alert(`Message send successfully to ${user.value}`);
+  }
 });
